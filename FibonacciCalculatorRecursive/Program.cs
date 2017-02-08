be3062ed63d8266eb4bfdb09c5fad447ca4283a4
kvoth45 <ralph.e.beard.iv@outlook.com>
2017-02-07 21:30:13 -0500

    initial commit

---
+++ FibonacciCalculatorRecursive/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciCalculatorRecursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number of times you want the formula to calculate:  ");
+            int upperNumber = int.Parse(Console.ReadLine());
+            Fibonacci(0, 1, 1, upperNumber);
+            Console.ReadLine();
+        }
+
+        public static void Fibonacci(int a, int b, int counter, int upperNumber)
+        {
+            Console.WriteLine(a);
+            if (counter < upperNumber)
+                Fibonacci(b, a + b, counter + 1, upperNumber);
+        }
+    }
+}
